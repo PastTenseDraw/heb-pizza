95d317df9d1f728cd250d09c6cffe6fd05a26ed8
PastTenseDraw <drew.baker.97@gmail.com>
2023-02-14 18:51:39 -0600

    Switching design systems, building navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarModule } from './navbar/navbar.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule,
+    NavbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
